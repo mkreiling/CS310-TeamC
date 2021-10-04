5751d78ff8aeca80c48ece968f5ad4ffbb66321d
Mattk <mkreiling@stu.jsu.edu>
2021-10-04 12:08:26 -0500

    Test Commit 1

--- src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/TAS.java
@@ -3,7 +3,7 @@ package edu.jsu.mcis.cs310.tas_fa21;
 public class TAS {
 
     public static void main(String[] args) {
-        Badge b = new Badge("Sephen", "D.", "Littlefield", "198" );
+       Badge b = new Badge("Sephen", "D.", "Littlefield", "198" );
        System.out.println(b.toString());
        b.setFname("Coolman");
        System.out.println(b.toString());
