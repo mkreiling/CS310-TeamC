67687a212728df08a678d0ba4a2f6f70233ad904
tmwijage <idrisatajuddin@gmail.com>
2021-10-06 11:24:40 -0500

    Punch modified

--- src/edu/jsu/mcis/cs310/tas_fa21/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa21/Punch.java
@@ -170,109 +170,17 @@ public class Punch {
         
         return t;
     }
+
+    Object printOriginal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
     
 
     /**
      *
      * @param s a Shift that represents the shift that should be adjusted
      */
-    public void adjust(Shift s){
-        
-        LocalTime shiftStart = s.getStartingTime();
-        LocalTime shiftStop = s.getStoppingTime();
-        LocalTime lunchStart = s.getLunchStart();
-        LocalTime lunchStop = s.getLunchStop();
-        
-        int gracePeriod = s.getGracePeriod();
-        int interval = s.getInterval();
-        int dock = s.getDock();
-        int lunchDeduct = s.getlunchDeduct();
-        
-        LocalDateTime punchTimeStamp = originalTimeStamp.toLocalDateTime();
-        LocalTime punchTime = punchTimeStamp.toLocalTime();
-        int punchTimeSeconds =  punchTime.getSecond();
-        int totalpunchTimeMinutes = punchTime.getMinute() + (punchTime.getHour()*60);
-        int totalshiftStopMinutes = shiftStop.getMinute() + (shiftStop.getHour()*60);
-        int totalshiftStartMinutes = shiftStart.getMinute() + (shiftStart.getHour()*60);
-        boolean weekend = (punchTimeStamp.getDayOfWeek().toString().equals("SATURDAY") || 
-                punchTimeStamp.getDayOfWeek().toString().equals("SUNDAY") );
-        
-        adjustedTimeStamp = Timestamp.valueOf(punchTimeStamp.withSecond(0));
-        
-        switch(this.punchTypeID){
-            
-            case 0:
-                
-                // CHECKS IF THE PUNCH IS CLOCKOUT FOR THE LUNCH BREAK
-           
-                if( punchTime.isAfter(shiftStart) && punchTime.isBefore(lunchStop) && (!weekend) ) {
-                  
-                    if( punchTime.isAfter(lunchStart) && punchTime.isBefore(lunchStop)) {
-                        adjustMessage = "Lunch Start";
-                        adjustedTimeStamp = helperMethod2(punchTimeStamp, lunchStart);                       
-                    }
-                    
-                }
-                
-                else if(weekend) {
-                    
-                    if(punchTime.isBefore(shiftStop) ) {
-                        
-                        if( (totalshiftStopMinutes - totalpunchTimeMinutes) <= gracePeriod ) {
-                            adjustMessage = "Shift Stop";
-                            adjustedTimeStamp = helperMethod2(punchTimeStamp, shiftStop);
-                        }
-                        
-                        else if ( (totalshiftStopMinutes - totalpunchTimeMinutes) > gracePeriod && 
-                                (totalshiftStopMinutes - totalpunchTimeMinutes) <= dock ) {
-                            adjustMessage = "Shift Dock";
-                            totalshiftStopMinutes = totalshiftStopMinutes - dock;
-                            adjustedTimeStamp = helperMethod1(punchTimeStamp, totalshiftStopMinutes);  
-                        }
-                        
-                        else {
-                            adjustMessage = "Interval Round";
-                            int timeInterval = totalshiftStopMinutes - totalpunchTimeMinutes;
-                            int quotient = timeInterval/interval;
-                            int remainder = timeInterval%interval;
-                            if(remainder <= 8) {
-                                totalshiftStopMinutes = totalshiftStopMinutes - (quotient*interval);
-                                adjustedTimeStamp = helperMethod1(punchTimeStamp, totalshiftStopMinutes);
-                            }
-                            else {
-                                totalshiftStopMinutes = totalshiftStopMinutes - ((quotient+1)*interval);
-                                adjustedTimeStamp = helperMethod1(punchTimeStamp, totalshiftStopMinutes);
-                            }
-                        }
-                    }
-                    
-                    else if(punchTime.isAfter(shiftStop) ) { 
-                        
-                        int timeInterval = totalpunchTimeMinutes - totalshiftStopMinutes;
-                        int quotient = timeInterval/interval;
-                        int remainder = timeInterval%interval;
-                        
-                        if(timeInterval <= interval) {
-                            
-                            if( (remainder == 0) && punchTime.getSecond() < 60) {
-                                adjustMessage = "None";
-                            }
-                            else {
-                                adjustMessage = "Shift Stop";
-                            }
-                            totalpunchTimeMinutes = totalpunchTimeMinutes - (quotient*interval);
-                            adjustedTimeStamp = helperMethod1(punchTimeStamp, totalshiftStopMinutes);
-                        }
-                        else {
-                            adjustMessage = "Interval Round";
-                            totalpunchTimeMinutes = totalpunchTimeMinutes - ((quotient+1)*interval);
-                            adjustedTimeStamp = helperMethod1(punchTimeStamp, totalpunchTimeMinutes);
-                        }
-                        
-                    }
-
-}
-        }
-    }
-    
+  
 }
